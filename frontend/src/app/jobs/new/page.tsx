61010f2f60c499358688bb13c582d7a1a53adb45
Patrick Tan <7201310+tanpatrick@users.noreply.github.com>
2025-02-17 22:14:45 +1300

    frontend: Fix page titles

--- frontend/src/app/jobs/new/page.tsx
+++ frontend/src/app/jobs/new/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
+
 import { NewJobPage } from "./NewJobPage";
+
+export const metadata: Metadata = {
+  title: "giigle - New Job",
+};
 
 export default function Page() {
   return <NewJobPage />;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
+
 import { HomePage } from "./HomePage";
+
+export const metadata: Metadata = {
+  title: "giigle - Jobs",
+};
 
 export default function Page() {
   return <HomePage />;
