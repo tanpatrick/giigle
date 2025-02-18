6ab72721ce5375d84af61f5dedb4200fe72cbfbc
Patrick Tan <7201310+tanpatrick@users.noreply.github.com>
2025-02-18 23:26:21 +1300

    frontend: Improve loading component for homepage

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,4 +1,18 @@
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <h1>LOADING...</h1>;
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-[30%,70%]">
+        <div className="border-r" style={{ height: `calc(100vh - 4rem)` }}>
+          <div className="w-full">
+            <div className="p-2 space-y-2 animate-pulse">
+              {Array.from({ length: 20 }).map((_, i) => (
+                <div key={i} className="h-20 p-5 bg-gray-200 rounded-md"></div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="bg-white "></div>
+      </div>
+    </div>
+  );
 }
