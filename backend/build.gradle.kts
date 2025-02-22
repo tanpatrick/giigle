02bce8cccc750c631d0f257ac6dca4c4e844b83d
Patrick Tan <7201310+tanpatrick@users.noreply.github.com>
2025-02-22 13:05:40 +1300

    backend: Implement in-memory /gigs endpoint + api docs

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.Embeddable")
     annotation("jakarta.persistence.MappedSuperclass")
+}
+
+configure<SourceSetContainer> {
+    java.sourceSets {
+        named("main") {
+            java.srcDirs(
+                "${layout.buildDirectory.get()}/generated/src/main/java",
+            )
+        }
+    }
 }
 
 dependencies {
@@ -9,11 +21,15 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -32,16 +48,53 @@ kotlin {
     }
 }
 
+openApiGenerate {
+    configOptions.set(
+        mapOf(
+            "annotationLibrary" to "swagger2",
+            "documentationProvider" to "springdoc",
+            "enumPropertyNaming" to "UPPERCASE",
+            "exceptionHandler" to "false",
+            "interfaceOnly" to "true",
+            "omitGradleWrapper" to "false",
+            "openApiNullable" to "false",
+            "requestMappingMode" to "api_interface",
+            "useBeanValidation" to "false",
+            "useSpringController" to "true",
+            "useSpringBoot3" to "true",
+            "useTags" to "true",
+        ),
+    )
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/api_specs/api.yaml")
+    packageName.set("giggle.generated")
+    apiPackage.set("giggle.generate.api")
+    modelPackage.set("giggle.generate.model")
+    outputDir.set("${layout.buildDirectory.get()}/generated")
+}
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    val kotlinVersion = "1.9.25"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.10.0"
     id("org.springframework.boot") version "3.4.3"
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn("openApiGenerate")
+}
+
+tasks.withType<KotlinCompile> {
+    mustRunAfter("openApiGenerate")
 }
 
 tasks.withType<Test> {
